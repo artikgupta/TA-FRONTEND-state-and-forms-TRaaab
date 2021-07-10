d208037e1d936d26d223f73be09b16c72e3a41f7
artikgupta <artikgupta>
2021-07-10 11:09:44 +0530

    refactored

--- block-BRaabm/code/movie-list/src/App.js
+++ block-BRaabm/code/movie-list/src/App.js
@@ -1,82 +1,85 @@
-import React from "react"
-import data from "./data.json"
-import "./style.css"
+import React from "react";
+import data from "./data.json";
+import "./style.css";
 
-class App extends React.Component{
-    constructor(props){
-        super(props)
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      active: false,
+      data: null,
+    };
+  }
 
-        this.state={
-        active:false
+  handleModal = (data) => {
+    this.setState({
+      active: true,
+      data,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <div className="box">
+          {data.map((movie) => {
+            console.log(movie);
+            return (
+              <>
+                <div className="card">
+                  <h1>{movie.Title}</h1>
+                  <img src={movie.Images} width="200px" />
+                  <p>{movie.Released}</p>
+                  <button
+                    className="btn"
+                    onClick={() => this.handleModal(movie)}
+                  >
+                    Read More
+                  </button>
+                </div>
+              </>
+            );
+          })}
+        </div>
+
+        {this.state.active ? (
+          <Modal
+            data={this.state.data}
+            state={this.state}
+            setState={this.setState.bind(this)}
+          />
+        ) : (
+          ""
+        )}
+      </>
+    );
+  }
+}
+
+function Modal({ data, state, setState }) {
+  return (
+    <div className="modal">
+      <h2>{data.Title}</h2>
+      <p>{data.Type}</p>
+      <p>{data.Writer}</p>
+      <p>{data.Year}</p>
+      <p>{data.imdbID}</p>
+      <p>{data.imdbRating}</p>
+      <p>{data.imdbVotes}</p>
+      <p>{data.totalSeasons}</p>
+      <button
+        onClick={() =>
+          setState({
+            active: false,
+            data: null,
+          })
         }
-    }
-
-   
-
-    handleModal=()=>{
-        this.setState({
-            active:true
-        })
-
-    }
-
-   
-
-
-render(){
-    return(
-        <>
-       < div className="box">
-       {
-             data.map((movie)=>{
-                console.log(movie)
-               return <>
-               <div className="card">
-               <h1>{movie.Title}</h1>
-               <img src ={movie.Images} width="200px"/>
-               <p>{movie.Released}</p>
-               <button className="btn" onClick={this.handleModal}>Read More</button>
-               </div>
-               </>
-            })
-       }
-       </div>
-
-      {function Modal(params) {
-
-       <div className="modal">
-     {     
-    data.map((ele)=>{
-        return <>
-
-        <h2>{ele.Title}</h2>
-        <p>{ele.Type}</p>
-        <p>{ele.Writer}</p>
-        <p>{ele.Year}</p>
-        <p>{ele.imdbID}</p>
-        <p>{ele.imdbRating}</p>
-        <p>{ele.imdbVotes}</p>
-        <p>{ele.totalSeasons}</p>
-        
-        
-        </>
-    })
+      >
+        X
+      </button>
+    </div>
+  );
 }
 
-</div>
-           
-       }
-    }
-
-     
-           {
-               this.state.active ? <Modal/> : ""
-           }
-       </>
-    )
-}
-
-}
-
-export default App
+export default App;
