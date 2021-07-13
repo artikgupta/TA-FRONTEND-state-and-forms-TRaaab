bc39fb98b38a76a65e8245970660476103e9bab4
artikgupta <artikgupta>
2021-07-13 23:19:12 +0530

    completed form-validation

---
+++ block-BRaabr/code/form-validation/src/App.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+import FromOne from "./components/FromOne"
+import FromTwo from "./components/FormTwo"
+
+function App(){
+
+    return (
+        <>
+   <FromOne />
+   <FromTwo />
+    
+        </>
+    )
+
+}
+
+   
+
+   
+
+
+export default App
