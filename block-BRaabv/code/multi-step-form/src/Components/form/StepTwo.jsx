ac1a3ea1b6b576da4d05dcc5fc10517c73c2cae2
artikgupta <artikgupta>
2021-07-15 17:12:08 +0530

    refactored

--- block-BRaabv/code/multi-step-form/src/Components/form/StepTwo.jsx
+++ block-BRaabv/code/multi-step-form/src/Components/form/StepTwo.jsx
@@ -3,7 +3,17 @@ import React from 'react'
 function StepTwo() {
     return (
         <div>
-           <input type="text" placeholder="StepTwo"  /> 
+            <h3>Message</h3>
+          <textarea name="" id="" cols="30" rows="10" placeholder="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."></textarea>
+
+
+        <label>The number one choice
+            <input type="checkbox" name="" id="" />
+        </label>
+        <label>The number two choice
+          <input type="checkbox" name="" id="" />
+        </label>
+
         </div>
     )
 }
